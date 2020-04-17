69448b24c77af6b74ac13c3701ec694abdfad53d
Gurman Grewal <10564765+Gurmanzg@users.noreply.github.com>
2020-04-16 23:42:31 -0700

    Week1 done
    hard

--- Week1/Week1/Week1.cpp
+++ Week1/Week1/Week1.cpp
@@ -17,51 +17,82 @@ struct Color {
 
         return Color{ dist(engine), dist(engine), dist(engine) };
     }
-
 };
 
 void colorSink(std::unique_ptr<Color> color) {
-    std::cout << "Success!\n";
+    // ... do nothing ... do not change the signature or implementation of this function
+    std::cout << "\nSuccess!\n";
 }
 
-ostream& operator<<(ostream& os, const Color& c) {
-    os << c.red << ',' << c.green << ',' << c.blue << "\n";
-    return os;
+ostream& operator<<(ostream& out, const Color& c)
+{
+    out << c.red << ", " << c.green << ", " << c.blue;
+    return out;
 }
 
 int main() {
 
     std::cout << "Hello, World\n";
-    auto c = Color::random();
-    std::cout << "First color is: " << c << "\n";
-    //#1 IS COMPLETE 
 
+    // 1. overload the output stream operator so that you can print this color directly to the output stream
+    auto c = Color::random();
+    std::cout << "\nFirst color is: " << c << "\n\n";
 
     // 2. here is a list of ten colors, print them out in order, and then in reverse order
-    std::cout << "Here I am showing what the original list is" << '\n';
     std::vector<Color> colors;
     for (int i = 0; i < 10; i++) {
-        Color::random();
         colors.push_back(Color::random());
-        std::cout << colors[i] << '\n';
     }
 
+    cout << "Colors initial order: \n";
+    for (Color& c : colors) {
+        cout << c << "\n";
+    }
 
-    std::cout << "Here is the REVERESED version of the list" << '\n';
-
-
+    cout << "\n\nColors reversed order\n";
+    reverse(colors.begin(), colors.end());
+    for (Color& c : colors)
+        cout << c << "\n";
     // 3. find the color in the list that is darkest and print it out
     // the darkest color is defined as the color where the sum of the red, green, and blue components is the smallest
 
+    int darkestColorIndex = 0;
+    int smallestValue = 1000; //stupid value lol
+    for (int i = 0; i < 10; i++) {
+        auto currentColorSum = colors.at(i).red + colors.at(i).green + colors.at(i).blue;
+        if (currentColorSum < smallestValue) {
+            smallestValue = currentColorSum;
+            darkestColorIndex++;
+        }
+    }
 
-
+    cout << "\nThe darkest color: " << colors.at((darkestColorIndex));
 
     // 4. order the colors from smallest 'blue' value to largest 'blue' value and print them out
+    std::vector<float> blueValues(10);
+    for (int i = 0; i < 10; i++) {
+        blueValues.at(i) = colors.at(i).blue;
+    }
 
+    std::sort(blueValues.begin(), blueValues.end());
+    std::vector<Color> properValues(10);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (blueValues.at(i) == colors.at(j).blue) {
+                properValues.at(i) = colors.at(j);
+            }
+        }
+    }
 
-
+    cout << "\n\nOrdered based off the color blue\n";
+    for (Color& c : properValues) {
+        cout << c << "\n";
+    }
 
     // 5. create a std::unique_ptr to a color on the free-store and call it 'colorPtr', pass this color to the
     // 'colorSink' function.
     // i.e. auto colorPtr = ...
+    cout << "\n\n";
+    unique_ptr<Color> colorPtr = unique_ptr<Color>(new Color());
+    colorSink(move(colorPtr));
 }
